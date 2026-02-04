7815ecb987feb43d2cc080bcfee1989ce0aca505
bhavanareddy675 <gudibhavanareddy@gmail.com>
2026-02-04 15:29:47 +0530

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math                    
+probabilities=[0.9,0.8,0.95]
+def cal_perplexity(prob):
+    entropy=sum([math.log2(p) for p in prob])/len(prob)
+    return math.pow(2,entropy)
+
+result=cal_perplexity(probabilities)
+print(result)
+
+
+# In[ ]:
+
+
+
+
